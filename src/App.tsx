a289ee48b8e5ab5677434f641cf1637c7bbde164
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-07-03 10:41:12 -0300

    Criando e aplicando os estilos globais utilizando o styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>GoBarber</h1>;
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>GoBarber</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
